890e43ac7f051d0e9bd0f7bee1b828fda4f9b434
punnyamk <punnyamk9847@gmail.com>
2025-09-27 23:44:22 +0530

    update backend url

--- frontend/src/server/api.js
+++ frontend/src/server/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-management-app-backend-ylum.onrender.com/",
+  baseURL: "https://task-management-app-backend-ylum.onrender.com/api",
 });
 
 export default api;
